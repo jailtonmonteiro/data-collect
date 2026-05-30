124efbf335cd588e49df8062556b29f364307b6d
jailtonmonteiro <jailtonmonteiro147@gmail.com>
2026-05-30 13:00:31 -0300

    feat: Adição de requeriments, e criação de funçoes para busca de informação individual.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# data-collect
-Coleta de dados na Web
+# Data-collect
+Estudando coleta de dados na Web, acompanhando as aulas do [@teomewhy](https://www.youtube.com/@teomewhy), seguindo a playlist [Coleta de Dados](https://youtube.com/playlist?list=PLvlkVRRKOYFSrkOL-Bze-42pTdJIAj0_h&si=VWfU3FrSqvAgoaw0).
+#
+### Bibliotcas utilizadas
+- requests
+- beautifulsoap
+#
---
+++ collect.py
@@ -0,0 +1,53 @@
+# %%
+import requests
+from bs4 import BeautifulSoup
+
+def get_content(url):
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'pt-BR,pt;q=0.9',
+        'cache-control': 'max-age=0',
+        'priority': 'u=0, i',
+        'referer': 'https://www.residentevildatabase.com/personagens/',
+        'sec-ch-ua': '"Chromium";v="148", "Google Chrome";v="148", "Not/A)Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36',
+    }
+    resp =  requests.get(url, headers=headers)
+    return resp
+
+def get_basic_infos(soup):
+    div_page = soup.find('div', class_='td-page-content')
+    paragrafo = div_page.find_all('p')[1]
+    ems = paragrafo.find_all('em')
+    data = {}
+    for i in ems:
+        chave, valor = i.text.split(':')
+        chave = chave.strip(' ')
+        data[chave] = valor.strip(' ')
+    return data
+
+def get_aparicoes(soup):
+    lis = ( soup.find('div', class_='td-page-content')
+                .find('h4')
+                .find_next()
+                .find_all('li'))
+    aparicoes = [i.text for i in lis]
+    return aparicoes
+# %%
+url = 'https://www.residentevildatabase.com/personagens/ada-wong/'
+resp = get_content(url)
+if resp.status_code != 200:
+    print('Nao foi possivel obter os dados')
+else:
+    soup = BeautifulSoup(resp.text)
+    data = get_basic_infos(soup)
+    data['Aparicoes'] = get_aparicoes(soup)
+data
+# %%
